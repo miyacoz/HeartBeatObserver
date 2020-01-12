7947ea9fb23ef8355f6c4ac75d0450e2c6a58be6
miyacoz <z@zeppel.biz>
2020-01-12 17:49:35 +0900

    added memory usage infos

--- heartbeatobserver.py
+++ heartbeatobserver.py
@@ -7,6 +7,7 @@ import os
 import sys
 
 import dotenv
+import psutil
 import requests
 
 def getWebhookUrl():
@@ -52,6 +53,30 @@ def isOkayStatus(statusCode):
   except:
     return False
 
+def getMemoryUsage(data):
+  def withUnit(n):
+    if n > 1024 ** 3:
+      g = n // (1024 ** 3)
+      m = math.floor((n - (g * (1024 ** 3))) // (1024 ** 2) / 10)
+      return str(g) + "." + str(m) + " G"
+    if n > 1024 ** 2:
+      m = n // (1024 ** 2)
+      k = math.floor((n - (m * (1024 ** 2))) // (1024 ** 1) / 10)
+      return str(m) + "." + str(k) + " M"
+    if n > 1024 ** 1:
+      m = n // (1024 ** 1)
+      d = math.floor((n - (k * (1024 ** 1))) // (1024 ** 0) / 10)
+      return str(k) + "." + str(d) + " K"
+    return str(n)
+  def isKeyAllowed(k):
+    allowedKeys = ["total", "available", "percent", "free"]
+    try:
+      allowedKeys.index(k)
+      return True
+    except ValueError:
+      return False
+  return ", ".join(["`" + k + ": " + withUnit(v) + "`" for k, v in data._asdict().items() if isKeyAllowed(k)])
+
 def getMessage():
   # make this more testable
   users = getPingedUsers()
@@ -61,6 +86,8 @@ def getMessage():
     (users if len([record[1] for record in result if not isOkayStatus(record[1])]) > 0 else ""),
     "> " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     "Current loads: " + ", ".join(["`" + la + "`" for la in loadAverages]),
+    "Virtual memory usage: " + getMemoryUsage(psutil.virtual_memory()),
+    "Swap memory usage: " + getMemoryUsage(psutil.swap_memory()),
     "Availability checks:",
     "\n".join([record[0] + " " + (str(record[1]) if isOkayStatus(record[1]) else "__" + str(record[1]) + "__") for record in result]),
   ])
